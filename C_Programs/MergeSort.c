29f585463a26598af1738ae7ecfd3f4348677550
Kiran Kurian <72328960+ottaplackan@users.noreply.github.com>
2021-02-23 23:49:14 +0530

    Update and rename MergeSort.txt to MergeSort.c

--- C_Programs/MergeSort.c
+++ C_Programs/MergeSort.c
@@ -1,49 +1,50 @@
 #include<stdio.h> 
 #define MAX 20 
-void mergeSort(int arr[],int low,int mid,int high) 
-{ 
+
+void mergeSort(int arr[],int low,int mid,int high) { 
 int i,m,k,l,temp[MAX]; 
 l=low; 
 i=low; 
 m=mid+1; 
-while((l<=mid)&&(m<=high)) // merging two sorted arrays 
-{if(arr[l]<=arr[m]) 
-{ 
+while((l<=mid)&&(m<=high)) { // merging two sorted arrays 
+if(arr[l]<=arr[m]) { 
 temp[i]=arr[l]; 
 l++; 
 } 
-else 
-{ 
+else { 
 temp[i]=arr[m]; 
 m++; 
 } 
-i++;} 
-if(l>mid) 
-{for(k=m;k<=high;k++) 
-{ 
+i++;
+} 
+if(l>mid) {
+for(k=m;k<=high;k++) { 
 temp[i]=arr[k]; 
 i++; 
-}} 
-else 
-{for(k=l;k<=mid;k++) 
+}
+} 
+else {
+for(k=l;k<=mid;k++) 
 {temp[i]=arr[k]; 
 i++; 
-}} 
-for(k=low;k<=high;k++) 
-{arr[k]=temp[k]; 
+}
+} 
+for(k=low;k<=high;k++) {
+arr[k]=temp[k]; 
 } 
 } 
-void partition(int arr[],int low,int high) 
-{int mid; 
-if(low<high) 
-{mid=(low+high)/2; 
+
+void partition(int arr[],int low,int high) {
+int mid; 
+if(low<high) {
+mid=(low+high)/2; 
 partition(arr,low,mid); 
 partition(arr,mid+1,high); 
 mergeSort(arr,low,mid,high); 
 } 
 } 
-void main() 
-{ 
+
+void main() { 
 int ar[MAX], i,n; 
 printf("Enter number of elements:"); 
 scanf("%d",&n); 
