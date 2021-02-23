17a4e88f4c103dfee2b73ad4ce82558d6263565a
Kiran Kurian <72328960+ottaplackan@users.noreply.github.com>
2021-02-23 23:34:41 +0530

    Update and rename ExpEval.txt to ExpressionEvaluation.c

--- C_Programs/ExpressionEvaluation.c
+++ C_Programs/ExpressionEvaluation.c
@@ -1,38 +1,35 @@
 #include<stdio.h> 
+
 int stack[20]; 
 top=-1; 
-void push(int x) 
-{ 
+
+void push(int x) { 
 top++; 
 stack[top]=x; 
 } 
-int pop() 
-{ 
+
+int pop() { 
 int x=stack[top]; 
 top--; 
 return x; 
 } 
-void main() 
-{ 
+
+void main() { 
 char exp[30]; 
 int i,j,k; 
 printf("Enter the postfix expression: "); 
 fflush(stdout); 
 gets(exp); 
-for(i=0;exp[i]!='\0';i++) 
-{ 
-if(isalpha(exp[i])) 
-{ 
+for(i=0;exp[i]!='\0';i++) { 
+if(isalpha(exp[i])) { 
 printf("Enter the value of %c: ",exp[i]); 
 scanf("%d",&j); 
 push(j); 
 } 
-else 
-{ 
+else { 
 j=pop(); 
 k=pop(); 
-switch(exp[i]) 
-{ 
+switch(exp[i]) { 
 case '+': push(j+k); 
 break; 
 case '-': push(k-j); 
