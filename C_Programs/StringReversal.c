4c1240f65b6c4aa5bff778a8cb8f5c05f54f6745
Kiran Kurian <72328960+ottaplackan@users.noreply.github.com>
2021-02-24 00:06:58 +0530

    Update and rename StringRev.txt to StringReversal.c

--- C_Programs/StringReversal.c
+++ C_Programs/StringReversal.c
@@ -1,30 +1,37 @@
 #include<stdio.h> 
+
 struct node { 
 int data; 
 struct node *addr; 
 }; 
+
 struct node *top = NULL; 
+
 struct node *createnode(char item) { 
 struct node *nn; 
 nn = (struct node*)malloc(sizeof(struct node)); 
 nn->data = item; 
 return nn; 
 } 
+
 void push(struct node *nn, char item) { 
 nn = createnode(item); 
 if(top == NULL) { 
 nn->addr = NULL; 
 top = nn; 
-} else { 
+} 
+else { 
 nn->addr = top; 
 top = nn; 
 } 
 } 
+
 char pop() { 
 char str; 
 if(top == NULL) { 
 printf("\nStack is Empty!"); 
-} else { 
+} 
+else { 
 struct node *p=top; 
 str = p->data; 
 p=p->addr; 
@@ -32,6 +39,7 @@ top=p;
 } 
 return str; 
 } 
+
 void main() { 
 struct node *nn; 
 char item, str[100]; 
