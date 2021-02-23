dec22770d07e64e3a4d73dae18e6e3b8a4390185
Kiran Kurian <72328960+ottaplackan@users.noreply.github.com>
2021-02-23 23:37:11 +0530

    Update and rename HeapSort.txt to HeapSort.c

--- C_Programs/HeapSort.c
+++ C_Programs/HeapSort.c
@@ -1,14 +1,11 @@
 #include<stdio.h> 
-void create_heap(int ar[], int size) 
-{ 
-int p,temp,i; 
-for(i=size;i>=1;i--) 
-{ 
+
+void create_heap(int ar[], int size) { 
+int p, temp, i; 
+for(i=size;i>=1;i--) { 
 p=i/2; 
-while(p>=1) 
-{ 
-if(ar[p]<ar[i]) 
-{ 
+while(p>=1) { 
+if(ar[p]<ar[i]) { 
 temp=ar[p]; 
 ar[p]=ar[i]; 
 ar[i]=temp; 
@@ -17,23 +14,21 @@ p=p/2;
 } 
 } 
 } 
-void rebuild_heap(int ar[], int size) 
-{ 
+
+void rebuild_heap(int ar[], int size) { 
 int lc,rc,temp,p=1,large; 
-while(p<=size) 
-{ 
+while(p<=size) { 
 lc=2*p; 
 rc=2*p+1; 
-if(lc<=size) 
-{ if(ar[lc]<ar[p]) 
+if(lc<=size) { 
+if(ar[lc]<ar[p]) 
 large=p; 
 else 
 large=lc; 
 } 
 if(rc<=size && ar[large]<ar[rc]) 
 large=rc; 
-if(p!=large) 
-{ 
+if(p!=large) { 
 temp=ar[p]; 
 ar[p]=ar[large]; 
 ar[large]=temp; 
@@ -43,9 +38,9 @@ else
 break; 
 } 
 } 
-void main() 
-{ 
-int ar[20],size,i,temp,size_backup; 
+
+void main() { 
+int ar[20], size, i, temp, size_backup; 
 printf("enter number of elements:"); 
 scanf("%d",&size); 
 printf("enter elements:"); 
@@ -53,8 +48,7 @@ for(i=1;i<=size;i++)
 scanf("%d",&ar[i]); 
 create_heap(ar, size); 
 size_backup=size; 
-while(size>1) 
-{ 
+while(size>1) { 
 rebuild_heap(ar,size); 
 temp=ar[1]; 
 ar[1]=ar[size]; 
