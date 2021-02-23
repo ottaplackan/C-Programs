a70fd36e2393f987bbd836eb45e6ed6c36263963
Kiran Kurian <72328960+ottaplackan@users.noreply.github.com>
2021-02-24 00:01:36 +0530

    Update and rename QuickSort.txt to QuickSort.c

--- C_Programs/QuickSort.c
+++ C_Programs/QuickSort.c
@@ -1,18 +1,19 @@
 #include<stdio.h> 
-void quick_sort(int arr[], int first, int last){ 
+
+void quick_sort(int arr[], int first, int last) { 
 int i, j, pivot, temp; 
-if(first < last){ 
+if(first < last) { 
 pivot = first; 
 i = first; 
 j = last; 
-while(i < j){ 
-while(arr[i]<=arr[pivot] && i<last){ 
+while(i < j) { 
+while(arr[i]<=arr[pivot] && i<last) { 
 i++; 
 } 
 while(arr[j] > arr[pivot]) { 
 j--; 
 } 
-if(i < j){ 
+if(i < j) { 
 temp = arr[i]; 
 arr[i] = arr[j]; 
 arr[j] = temp;
@@ -25,12 +26,13 @@ quick_sort(arr, first, j-1);
 quick_sort(arr, j+1, last); 
 } 
 } 
-void main(){ 
+
+void main() { 
 int n, ar[20]; 
 printf("Enter the number of elements in the array: "); 
 scanf("%d",&n); 
 printf("Enter the elements in the array: "); 
-for(int i=0;i<n;i++){ 
+for(int i=0;i<n;i++) { 
 scanf("%d",&ar[i]); 
 } 
 printf("\nBefore Sorting: "); 
@@ -41,4 +43,5 @@ quick_sort(ar, 0, n-1);
 printf("\n\nAfter Sorting: "); 
 for(int i=0;i<n;i++){ 
 printf("%d ",ar[i]); 
-}} 
+}
+} 
